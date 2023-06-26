2777795333a0889ef76ddd07393c68b75ceda448
deepak-bits <deepak.kumar3112@gmail.com>
2023-06-26 18:16:01 +0530

    Refactored using promises

--- fluxkart.js
+++ fluxkart.js
@@ -1,4 +1,263 @@
 const express = require('express');
+const mysql = require('mysql2/promise');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+
+const queries = {
+    createTable: `
+        CREATE TABLE IF NOT EXISTS Contact (
+            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            phoneNumber VARCHAR(255),
+            email VARCHAR(255),
+            linkedId INT,
+            linkPrecedence ENUM("secondary", "primary"),
+            createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updatedAt TIMESTAMP NOT NULL,
+            deletedAt TIMESTAMP
+        )
+    `,
+    getContactsByEmail: 'SELECT * FROM Contact WHERE email = ? AND linkPrecedence = "primary"',
+    getContactsByPhone: 'SELECT * FROM Contact WHERE phoneNumber = ? AND linkPrecedence = "primary"',
+    changePrimaryToSecondary: 'UPDATE Contact SET linkPrecedence = "secondary", linkedId = ?, updatedAt = CURRENT_TIMESTAMP WHERE id = ?',
+    getContactsByLinkId: 'SELECT * FROM Contact WHERE linkedId = ?',
+    getSecondaryContactsByLinkId: 'SELECT * FROM Contact WHERE linkedId = ? AND linkPrecedence = "secondary"',
+    addNewSecondaryContact: 'INSERT INTO Contact (phoneNumber, email, linkedId, linkPrecedence, updatedAt) VALUES (?, ?, ?, "secondary", CURRENT_TIMESTAMP)',
+    checkSecondaryContact: 'SELECT * FROM Contact WHERE email = ? AND phoneNumber = ? AND linkPrecedence = "secondary"',
+    checkPrimaryContact: 'SELECT * FROM Contact WHERE id = ? AND linkPrecedence = "primary"',
+    createNewPrimaryContact: 'INSERT INTO Contact (phoneNumber, email, linkedId, linkPrecedence, updatedAt) VALUES (?, ?, NULL, "primary", CURRENT_TIMESTAMP)'
+};
+
+async function main() {
+    // create the connection
+    // const con = await mysql.createConnection({
+    //     host:'localhost', 
+    //     user: 'root',
+    //     password: '', 
+    //     database: 'fluxKartDb', 
+    // });
+
+    const con = await mysql.createConnection({
+        host: 'sql12.freesqldatabase.com',
+        user: 'sql12628584',
+        password: 'NgGAAUVzLk',
+        database: 'sql12628584'
+    });
+
+    console.log('MySQL connected...');
+
+    // create table if not exists
+    try {
+        await con.execute(queries.createTable);
+        console.log("Table created");
+    } catch(err) {
+        throw err;
+    }
+
+    app.post('/identify', async (req, res) => {
+        try {
+            let { email, phoneNumber } = req.body;
+            phoneNumber = phoneNumber.toString();
+    
+            if (!email && !phoneNumber) {
+                return res.status(400).json({ error: 'Email or phoneNumber is required' });
+            }
+    
+            let primaryContactId;
+            let primaryContactIdEmail;
+            let primaryContactIdPhone;
+            const emails = [];
+            const phoneNumbers = [];
+            const secondaryContactIds = [];
+    
+            let emailObj;
+            let phoneNumberObj;
+    
+    
+            try {
+                await retrievePrimaryContact();
+            } catch (err) {
+                console.log('Error retrieving primary contact:', err);
+            }
+    
+            await updatePrimaryContactIdBothAvailable();
+            await updatePrimaryContactIdOnlyOneAvailable();
+            await createPrimaryContactWhenFoundNothing();
+            
+            const contact = {
+                primaryContactId,
+                emails,
+                phoneNumbers,
+                secondaryContactIds
+            };
+    
+            return res.json({ contact });
+    
+            // Find primary contact based on email or phoneNumber
+            async function retrievePrimaryContact() {
+                if (email) {
+                    const [rows] = await con.execute(queries.getContactsByEmail, [email]);
+                    if (rows.length > 0) {
+                        emailObj = rows[0];
+                        primaryContactIdEmail = rows[0].id;
+                        !emails.includes(rows[0].email) && emails.push(rows[0].email);
+                        !phoneNumbers.includes(rows[0].phoneNumber) && phoneNumbers.push(rows[0].phoneNumber);
+                    }
+                }
+        
+                if (phoneNumber) {
+                    const [rows] = await con.execute(queries.getContactsByPhone, [phoneNumber]);
+                    if (rows.length > 0) {
+                        phoneNumberObj = rows[0];
+                        primaryContactIdPhone = rows[0].id;
+                        !emails.includes(rows[0].email) && emails.push(rows[0].email);
+                        !phoneNumbers.includes(rows[0].phoneNumber) && phoneNumbers.push(rows[0].phoneNumber);
+                    }
+                }
+            }
+    
+            /**
+             * Update primary contact id when both email and phone number are available 
+             * Also, change primary to secondary if needed
+             */
+            async function updatePrimaryContactIdBothAvailable() {
+                if(primaryContactIdEmail && primaryContactIdPhone) {
+                    if(primaryContactIdEmail === primaryContactIdPhone) {
+                        primaryContactId = primaryContactIdEmail;
+                    } else {
+                        const time1 = emailObj.createdAt;
+                        const time2 = phoneNumberObj.createdAt;
+        
+                        if(time1.getTime() < time2.getTime()) {
+                            // email object was added earlier
+                            try {
+                                const [rows] = await con.execute(queries.changePrimaryToSecondary, [emailObj.id, phoneNumberObj.id]);
+                                primaryContactId = emailObj.id;
+                                secondaryContactIds.push(phoneNumberObj.id);
+                                console.log('LinkPrecedence updated successfully');
+                            } catch (err) {
+                                console.error('Error updating linkPrecedence:', err);
+                            }                       
+                        } else {
+                            // phoneNumber object was added earlier
+                            try {
+                                const [rows] = await con.execute(queries.changePrimaryToSecondary, [phoneNumberObj.id, emailObj.id]);
+                                primaryContactId = emailObj.id;
+                                secondaryContactIds.push(phoneNumberObj.id);
+                                console.log('LinkPrecedence updated successfully');
+                            } catch (err) {
+                                console.error('Error updating linkPrecedence:', err);
+                            }   
+                        }
+                    }
+                }
+            }
+
+            /**
+             * Update primary contact id when either email or phone number is available 
+             * Also, check for secondary contact, if not found create it
+             */
+            async function updatePrimaryContactIdOnlyOneAvailable() {
+                if(primaryContactIdEmail && !primaryContactIdPhone) primaryContactId = primaryContactIdEmail;
+                if(primaryContactIdPhone && !primaryContactIdEmail) primaryContactId = primaryContactIdPhone;
+    
+                if (primaryContactId) {
+                    try {
+                        const [rows] = await con.execute(queries.getContactsByLinkId, [primaryContactId]);
+                        rows.forEach((row) => {
+                            !emails.includes(row.email) && emails.push(row.email);
+                            !phoneNumbers.includes(row.phoneNumber) && phoneNumbers.push(row.phoneNumber);
+                            secondaryContactIds.push(row.id);
+                        });
+                    } catch (err) {
+                        console.error('Error retrieving secondary contacts:', err);
+                    }
+    
+                    const [rows] = await con.execute(queries.getSecondaryContactsByLinkId, [primaryContactId]);
+                    if(rows.length == 0) {
+                        try {
+                            const [result] = await con.execute(queries.addNewSecondaryContact, [phoneNumber, email, primaryContactId]);
+                            !emails.includes(email) && emails.push(email);
+                            !phoneNumbers.includes(phoneNumber) && phoneNumbers.push(phoneNumber);
+                            secondaryContactIds.push(result.insertId);
+                        } catch (err) {
+                            console.error('Error creating new secondary contact:', err);
+                        }
+                    } else return;
+                }
+            }
+    
+            /**
+             * Update primary contact id when both email and phone number are NOT available 
+             * Also, create the primary contact if it is not there
+             */ 
+            async function createPrimaryContactWhenFoundNothing() {
+                if(!primaryContactId) {
+                    const [results] = await con.execute(queries.checkSecondaryContact, [email, phoneNumber]);
+                    if(results.length > 0) {
+                        primaryContactId = results[0].linkedId;
+                        const [rows] = await con.execute(queries.checkPrimaryContact, [primaryContactId]);
+                        if(rows.length > 0) {
+                            !emails.includes(rows[0].email) && emails.push(rows[0].email);
+                            // console.log(emails);
+                            !phoneNumbers.includes(rows[0].phoneNumber) && phoneNumbers.push(rows[0].phoneNumber);
+                        }
+    
+                        !emails.includes(results[0].email) && emails.push(results[0].email);
+                        !phoneNumbers.includes(results[0].phoneNumber) && phoneNumbers.push(results[0].phoneNumber);
+                        secondaryContactIds.push(results[0].id);
+                    } else {
+                        try {
+                            const [result] = await con.execute(queries.createNewPrimaryContact, [phoneNumber, email]);
+                            primaryContactId = result.insertId;
+                            !emails.includes(email) && emails.push(email);
+                            !phoneNumbers.includes(phoneNumber) && phoneNumbers.push(phoneNumber);
+                        } catch (err) {
+                            console.error('Error creating new primary contact:', err);
+                        }   
+                    }
+                }
+            } 
+        } catch (err) {
+            console.error('Error processing contacts:', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+    });
+}
+
+main();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ----------------------- OLD Code ------------------------
+const express = require('express');
 const mysql = require('mysql2');
 const PORT = process.env.PORT || 3000;
 
@@ -301,4 +560,4 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-
+*/
